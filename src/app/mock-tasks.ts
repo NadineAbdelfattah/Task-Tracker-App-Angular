4accf157c03df2b926b3252494123fbeb3ac728d
NadineAbdelfattah <78588221+NadineAbdelfattah@users.noreply.github.com>
2021-05-21 21:21:19 +0200

    first commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Project Discussion',
+        day: 'May 5th at 2:20pm',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Team Meating',
+        day: 'May 10th at 2:20pm',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'My Birthday Party',
+        day: 'June 12th at 10:00 pm',
+        reminder: false
+    }
+]
